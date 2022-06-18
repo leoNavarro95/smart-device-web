c3d5b4796c3b9b21d5232066f9b6f6544f19ca49
leoNavarro95 <potty3800@gmail.com>
2022-06-18 11:09:57 -0500

    added pinia and tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="m-5">
+    <div class="text-3xl font-bold underline bg-pink-600">Hello tailwind</div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
+
