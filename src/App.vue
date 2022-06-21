4ee2803435b89f82deb51e097bf77e0c41e136cf
leoNavarro95 <potty3800@gmail.com>
2022-06-21 17:57:04 -0500

    Added NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Demo from './components/Demo.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <div class="m-5">
-    <div class="text-3xl font-bold underline bg-pink-600">Hello tailwind</div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Demo msg="Demo" />
-    
+  <div class="md:m-10">
+    <!-- TODO: Scaffold/layout needed here -->
+    <NavBar/>
+    <router-view></router-view>
+
   </div>
 </template>
 
