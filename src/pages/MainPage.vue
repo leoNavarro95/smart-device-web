792dae2c9ee0fd208696994f7ea61aecaac35672
leoNavarro95 <potty3800@gmail.com>
2022-06-21 17:58:09 -0500

    MainPage created

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,11 @@
+
+<script setup>
+// import StatusBar from '../components/StatusBar.vue'
+
+
+</script>
+
+<template>
+    <!-- <StatusBar msg="Flip Card Game"/> -->
+    <h1>MainPage</h1>
+</template>
