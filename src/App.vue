288030d637c54cc007aaf6475beebbe22553add7
leoNavarro95 <potty3800@gmail.com>
2022-06-21 16:56:03 -0500

    Renamed HelloWorld example to Demo included Vue Router in project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/Demo.vue'
 </script>
 
 <template>
   <div class="m-5">
     <div class="text-3xl font-bold underline bg-pink-600">Hello tailwind</div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <Demo msg="Demo" />
+    
   </div>
 </template>
 
