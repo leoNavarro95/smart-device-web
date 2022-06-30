1f2e9e7c556a7d402df27e20c641a3dfe3036ec7
leoNavarro95 <potty3800@gmail.com>
2022-06-30 10:34:08 -0500

    Using new action of mcu store

--- src/pages/Demo.vue
+++ src/pages/Demo.vue
@@ -1,54 +1,30 @@
 <script setup>
 import { ref } from 'vue'
 import { storeToRefs } from 'pinia'
-import { getCurrentInstance } from "vue"
 
 import Card from '../components/Card.vue'
 import DigitalStatus from '../components/DigitalStatus.vue'
 
-import useGpioStore from '../stores/gpio.js'
-
-const { proxy } = getCurrentInstance()
+import useMCUStore from '../stores/mcu.js'
 
 defineProps({
   msg: String
 })
 
-const count = ref(0)
+const mcu = useMCUStore()
+const {digitalOutput, digitalInput} = storeToRefs(mcu)
 
-const gpio = useGpioStore()
-
-const {digitalOutput, digitalInput} = storeToRefs(gpio)
-
-const sendGPIO = ( outPin ) => {
-   outPin.status = !outPin.status 
-   console.log(outPin.pin.value + ': ' + outPin.status.value);
-
-      let data = {
-        command: "setGPIO",
-        id: outPin.pin.value,
-        status: outPin.status.value
-      }
-
-      let json = JSON.stringify(data)
-      proxy.$socket.send(json)
-}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-  <div class="p-5">    
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="count++" >count is: {{ count }}</button>
-  </div>
 
   <div class="grid grid-cols-2 md:grid-cols-3 justify-center items-center">
     
     <card header="Outputs   +">
-      
       <div v-for="output in digitalOutput" >
-        <digital-status @change-status="sendGPIO(output)" type="Output" :pin="output.pin" :status="output.status"></digital-status>
+        <digital-status @change-status="mcu.changePinStatus(output)" type="Output" :pin="output.pin" :status="output.status"></digital-status>
       </div>
-
     </card>
     
     <card header="PWM State">
