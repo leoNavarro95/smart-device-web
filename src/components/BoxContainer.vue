2abb288670a7f9ca3d4f2828fdf422e85cc6bdfb
leoNavarro95 <potty3800@gmail.com>
2022-08-28 11:33:45 -0500

    Added collapse button functionality

--- src/components/BoxContainer.vue
+++ src/components/BoxContainer.vue
@@ -1,25 +1,30 @@
 <script setup>
+import { ref } from 'vue'
 
 defineProps({
   title: { type: String, default: "Empty title" }
 })
 
+let show = ref(true)
+
 </script>
 
 
 <template>
-  <div class="bg-white m-3 p-0 border border-indigo-400 rounded-lg">
+  <div class="bg-white m-3 p-0 border border-indigo-400 rounded-lg transition duration-300 ease-in-out">
     <div class="
           flex justify-between items-start
         bg-indigo-400 
           rounded-t-md 
           text-xl font-semibold font-mono text-white
-          mb-2 px-2 
-          ">
+          px-2 
+          "
+          :class="{'rounded-md': !show}"
+    >
       <div>{{ title }}</div>
 
       <!-- TODO Collapse button here -->
-      <button @click="$emit('addNewItem')" 
+      <button @click="show = !show" 
               class="
                 m-0 p-0 
                 rounded-full hover:bg-indigo-300
@@ -28,24 +33,24 @@ defineProps({
       </button>
     </div>
 
-    <!-- BODY: Default slot -->
-    <div class="p-1 md:p-5 text-gray-700">
-      <slot></slot>
-    </div>
-    <!-- /BODY -->
+    <div v-show="show" class="p-1 md:p-5 text-gray-700">
+      <!-- BODY: Default slot -->
+        <slot></slot>
+      <!-- /BODY -->
 
-    <!-- FOOTER: Footer slot -->
-    <div class="
-        bg-indigo-200
-          rounded-b-lg
-        text-white
-          px-2
-        ">
-      <slot name="footer">
+      <!-- FOOTER: Footer slot -->
+      <div class="
+          bg-indigo-200
+            rounded-b-lg
+          text-white
+            px-2
+          ">
+        <slot name="footer">
 
-      </slot>
+        </slot>
+      </div>
+      <!-- /FOOTER -->
     </div>
-    <!-- /FOOTER -->
 
   </div>
 </template>
